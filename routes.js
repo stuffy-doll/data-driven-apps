dd4e45f6b3afd96f944958200b570eb1e6acda75
Abel Tesfa <abeltesfa@gmail.com>
2022-05-26 15:38:42 -0400

    part 2 finished

--- routes.js
+++ routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('./db/models');
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+    res.render('index', { title: 'Home', books });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
