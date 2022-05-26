05dcdc46d0ef033631e6f284f976ad7296ea7882
Abel Tesfa <abeltesfa@gmail.com>
2022-05-26 17:09:54 -0400

    finished project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
@@ -8,6 +9,8 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 // Catch unhandled requests and forward to error handler.
